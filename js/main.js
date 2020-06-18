50fc5d9d838a527c52aab393404be0842eaecb1c
Lilian Ovrawah <lilianovrawah@gmail.com>
2020-06-18 08:06:30 +0100

    feat: index page

--- js/main.js
+++ js/main.js
@@ -1,12 +1,7 @@
-const menu = document.getElementById('menu');
-const nav = document.getElementById('nav');
-const exit = document.getElementById('exit');
 
-menu.addEventListener('click', function(e){
-    nav.classList.toggle('hide-mobile');
-    e.preventDefault()
-})
-exit.addEventListener('click', function(e){
-    nav.classList.add('hide-mobile');
-    e.preventDefault()
+$(document).ready(function(){
+    $('.menu-toggle').on('click', function(){
+        $('.nav').toggleClass('showing');
+        $('.nav ul').toggleClass('showing');
+    })
 })
