cbf45efde47f8168fb9251c9614902f8c10fd588
Lilian Ovrawah <lilianovrawah@gmail.com>
2020-06-19 20:15:34 +0100

    feat: index page

--- js/chart-section.js
+++ js/chart-section.js
@@ -1,71 +1,111 @@
-//Get the chart canvas
-const chartCanvas = document.getElementById('expenditure-chart');
+/*
+*   
+* INITIALISE AND DRAW CHART USING CHART.JS
+*
+*
+*/
 
-//Feed in chart data set
-let expenditureChart = new Chart(chartCanvas, {
-    type: 'line',
-    data: {
-        datasets: [
-            {fill: 'origin'},      // 0: fill to 'origin'
-            {fill: '+2'},          // 1: fill to dataset 3
-            {fill: 1},             // 2: fill to dataset 1
-            {fill: false},         // 3: no fill
-            {fill: '-2'}           // 4: fill to dataset 2
-        ],
-        labels: ['January', 
-                 'February', 
-                 'March', 
-                 'April', 
-                 'May', 
-                 'June', 
-                 'July', 
-                 'August', 
-                 'September', 
-                 'October', 
-                 'November', 
-                 'December'
-        ],
-        datasets: [{
-            label: 'Amount Spent (Billions)',
-            data: [12, 19, 3, 5, 2, 3, 12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
+let type = 'line';//Set chart type
+
+//Set chart data
+let data = {
+    labels: ['', '', '', '', '', '', '', '', '', '', '', ''],
+    //labels: ['J', 'F', 'M', 'A', 'M', 'J', 'J', 'A', 'S', 'O', 'N', 'D'],
+    datasets: [{
+        backgroundColor: 'rgba(0, 147, 93, 0.16)',
+        borderColor: 'rgba(0, 148, 94, 1)',//Area border color
+        borderWidth: 2,//Line width in pixels
+        data: [12, 9, 3, 5, 2, 3, 12, 2, 3, 50, 2, 3],//Chart data
+        fill: 'origin',
+        lineTension: 0,
+        pointBackgroundColor: 'rgba(51, 129, 100, 1)',
+        pointBorderColor: 'rgba(51, 129, 100, 1)',
+        pointBorderWidth: 2
+    }]
+};
+
+//Set chart display options
+let options = {
+    scales: { 
+        yAxes: [{
+            ticks: {
+                beginAtZero: true,
+                maxTicksLimit: 10,
+            },
+            scaleLabel: {
+                display: true,
+                labelString: 'Amount Spent (Billions)',
+                fontColor: '#474747'
+            }
+        }],
+        xAxes: [{
+            gridLines: {
+                offsetGridLines: true,
+                display: false,
+                tickMarkLength: 5
+            },
+            ticks: {
+                labelOffset: 5,
+                maxTicksLimit: 12
+            },
+            scaleLabel: {
+                display: false,
+                labelString: 'Months',
+                fontColor: '#474747'
+            }
         }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }, 
-        legend: {
-            display: false,
+    }, 
+    plugins: {
+        filler: {
+            propagate: true
         }
+    },
+    legend: {
+        display: false
     }
-});
+};
+
+const chartCanvas = document.getElementById('expenditure-chart');//Get the chart canvas
+
+let expenditureChart = new Chart(chartCanvas, {type, data, options});//Draw the chart
+
+
 
 /*
-let type = 'line';//Chart type
-let data = {};//Chart data
-let options = {};//Chart display options
-
-//Initialize the chart
-let expenditureChart = new Chart(type, data, options);
+*   
+* HANDLE DATA PRSENTATION - CHART/TABLE DISPLAY
+*
+*
 */
+
+const dataPresentation = document.getElementById('data-presentation');//Get data presentation div
+const chartButton = document.getElementById('chart-btn');//Get chart button
+const tableButton = document.getElementById('table-btn');//Get table button
+
+const chartIcon = chartButton.firstElementChild;//Get chart icon
+const tableIcon = tableButton.firstElementChild;//Get chart icon
+
+chartIcon.style.color = "#00945E";//Set chart icon color - active
+tableIcon.style.color = "#757575";//Set table icon color - inactive
+
+function toggleDataPresentation() {
+    if(event.srcElement.id == chartButton){
+        console.log("Chart Icon Present");
+        chartIcon.style.color = "#00945E";
+        tableIcon.style.color = "#757575";
+        //dataPresentation.display = ""
+    }
+    else if(event.srcElement.id == tableButton){
+        console.log("Table Icon Present");
+        chartIcon.style.color = "#757575";
+        tableIcon.style.color = "#00945E";
+        //dataPresentation.display = ""
+    }
+};
+
+/*
+chartButton.addEventListener("click", toggleDataPresentation);//Set chart button click listener
+tableButton.addEventListener("click", toggleDataPresentation);//Set table button click listener
+*/
+
+
