a382fe3b666db0c44d6d12f2e4a7bdf4851e76a7
nehamahto58 <nehamahto58@gmail.com>
2020-06-21 00:04:25 +0545

    feat/filter-popupAdded

--- js/ExpenditureScript.js
+++ js/ExpenditureScript.js
@@ -1,3 +1,36 @@
+var view_choice = "day";
+var sort_choice = "htl";
+function show_filter() {
+    document.getElementById(view_choice).style.backgroundColor = "#00945e";
+    document.getElementById(view_choice).style.color = "white";
+    document.getElementById(sort_choice).style.backgroundColor = "#00945e";
+    document.getElementById(sort_choice).style.color = "white";
+    document.getElementById("filter-screen").style.display = "block";
+}
+
+function filterSelectionView(view){
+    view_choice = view;
+    document.getElementById("day").removeAttribute("style");
+    document.getElementById("month").removeAttribute("style");
+    document.getElementById("year").removeAttribute("style");
+    document.getElementById(view).style.backgroundColor = "#00945e";
+    document.getElementById(view).style.color = "white";
+}
+function filterSelectionSort(sort){
+    sort_choice = sort;
+    document.getElementById("htl").removeAttribute("style");
+    document.getElementById("lth").removeAttribute("style");
+    document.getElementById(sort).style.backgroundColor = "#00945e";
+    document.getElementById(sort).style.color = "white";
+}
+function apply(){
+    document.getElementById("filter-screen").style.display = "none";
+    console.log("View By: ",view_choice); 
+    console.log("Sort By: ",sort_choice); 
+}
+
+
+
 
 let showPopup = function(){
     document.querySelector('.section-wrapper').classList.add('show')
