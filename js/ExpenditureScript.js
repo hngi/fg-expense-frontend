4f1ada8935ab8ba3636b629f9611f725df3788ac
Nwokochahannah <hannahsopuruchi@gmail.com>
2020-06-20 07:28:38 +0100

    bug: corrected JS

--- js/ExpenditureScript.js
+++ js/ExpenditureScript.js
@@ -1,9 +1,9 @@
-let profileSummary = document.querySelector(".profileSummary");
 
 let showPopup = function(){
     document.querySelector('.section-wrapper').classList.add('show')
 }
-profileSummary.addEventListener('click' , showPopup);
+let profileSummary = document.querySelectorAll(".profileSummary").forEach
+(profileSummary => profileSummary.addEventListener('click' , showPopup));
 
 let closeButton = document.querySelector('.section-wrapper .close-button');
 
